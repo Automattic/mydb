b1da939f4237039fc149261aaacdf891c3162241
Guillermo Rauch <rauchg@gmail.com>
2012-10-06 11:58:20 -0700

    test: initial suite

---
+++ test/mydb.js
@@ -0,0 +1,40 @@
+
+/**
+ * Test dependencies.
+ */
+
+var http = require('http').Server
+  , server = require('..')
+  , expect = require('expect.js')
+  , expose = require('mydb-expose')
+  , client = require('mydb-client');
+
+/**
+ * Test.
+ */
+
+describe('mydb', function(){
+
+  describe('exports', function(){
+    it('Server', function(){
+      expect(server).to.be.a('function');
+    });
+
+    it('Client', function(){
+      expect(server.Client).to.be.a('function');
+    });
+
+    it('Subscription', function(){
+      expect(server.Subscription).to.be.a('function');
+    });
+  });
+
+  describe('attaching', function(){
+    it('should work without `new`', function(){
+      var httpServer = http();
+      var mydb = server(httpServer);
+      expect(mydb).to.be.an(server);
+    });
+  });
+
+});
